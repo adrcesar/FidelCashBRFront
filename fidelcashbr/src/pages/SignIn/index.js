bea9f34432429a0fe87f778e0a38019e1d747923
Adriano Ferreira <adrcesar@gmail.com>
2020-07-23 23:27:14 -0300

    login 01

--- fidelcashbr/src/pages/SignIn/index.js
+++ fidelcashbr/src/pages/SignIn/index.js
@@ -5,7 +5,24 @@ import Logo from "../../assets/logo.jpeg";
 import api from "../../services/api";
 import { login } from "../../services/auth";
 
-import { Form, Container } from "./styles";
+//import useStyles from "../SignIn/styles";
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+//import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Container from '@material-ui/core/Container';
+
+
 
 class SignIn extends Component {
   state = {
@@ -33,29 +50,110 @@ class SignIn extends Component {
     }
   };
 
+  
+
   render() {
+
+    function useStyles(){
+      const useStyles = makeStyles((theme) => ({
+        paper: {
+          marginTop: theme.spacing(8),
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        },
+        avatar: {
+          margin: theme.spacing(1),
+          backgroundColor: theme.palette.secondary.main,
+        },
+        form: {
+          width: '100%', // Fix IE 11 issue.
+          marginTop: theme.spacing(1),
+        },
+        submit: {
+          margin: theme.spacing(3, 0, 2),
+        },
+      }));
+      return useStyles;
+    }
+    
+    //var classes = useStyles();
+
     return (
-      <Container>
-        <Form onSubmit={this.handleSignIn}>
-          <img src={Logo} alt="Airbnb logo" />
-          {this.state.error && <p>{this.state.error}</p>}
-          <input
-            type="text"
-            placeholder="Endereço de e-mail"
-            onChange={e => this.setState({ usuario: e.target.value })}
-          />
-          <input
-            type="password"
-            placeholder="Senha"
-            onChange={e => this.setState({ senha: e.target.value })}
-          />
-          <button type="submit">Entrar</button>
-          <hr />
-          <Link to="/signup">Criar conta grátis</Link>
-        </Form>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div >
+          <Avatar >
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form  noValidate>
+            {this.state.error && <p>{this.state.error}</p>}
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="usuario"
+              label="Usuário"
+              name="usuario"
+              autoFocus
+              onChange={e => this.setState({ usuario: e.target.value })}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="senha"
+              label="Senha"
+              type="password"
+              id="senha"
+              autoComplete="current-password"
+              onChange={e => this.setState({ senha: e.target.value })}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              
+              onClick={this.handleSignIn}
+            >
+              Sign In
+            </Button>
+          </form>
+        </div>
+        
       </Container>
     );
   }
 }
+
+
+/*return (
+  <Container>
+    <Form onSubmit={this.handleSignIn}>
+      <img src={Logo} alt="Airbnb logo" />
+      {this.state.error && <p>{this.state.error}</p>}
+      <input
+        type="text"
+        placeholder="Endereço de e-mail"
+        onChange={e => this.setState({ usuario: e.target.value })}
+      />
+      <input
+        type="password"
+        placeholder="Senha"
+        onChange={e => this.setState({ senha: e.target.value })}
+      />
+      <button type="submit">Entrar</button>
+      <hr />
+      <Link to="/signup">Criar conta grátis</Link>
+    </Form>
+  </Container>
+);*/
+
 
 export default withRouter(SignIn);
--- fidelcashbr/src/pages/SignIn/styles.js
+++ fidelcashbr/src/pages/SignIn/styles.js
@@ -1,63 +1,24 @@
-import styled from "styled-components";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
-export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-`;
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-export const Form = styled.form`
-  width: 400px;
-  background: #fff;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  img {
-    width: 100px;
-    margin: 10px 0 40px;
-  }
-  p {
-    color: #ff3333;
-    margin-bottom: 15px;
-    border: 1px solid #ff3333;
-    padding: 10px;
-    width: 100%;
-    text-align: center;
-  }
-  input {
-    flex: 1;
-    height: 46px;
-    margin-bottom: 15px;
-    padding: 0 20px;
-    color: #777;
-    font-size: 15px;
-    width: 100%;
-    border: 1px solid #ddd;
-    &::placeholder {
-      color: #999;
-    }
-  }
-  button {
-    color: #fff;
-    font-size: 16px;
-    background: #fc6963;
-    height: 56px;
-    border: 0;
-    border-radius: 5px;
-    width: 100%;
-  }
-  hr {
-    margin: 20px 0;
-    border: none;
-    border-bottom: 1px solid #cdcdcd;
-    width: 100%;
-  }
-  a {
-    font-size: 16;
-    font-weight: bold;
-    color: #999;
-    text-decoration: none;
-  }
-`;
+export default useStyles;
